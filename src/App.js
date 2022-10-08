e9a25ff5924f71f87211d0c9af555d7a5fc57a0b
Atchi <imeneatchi@gmail.com>
2022-10-08 22:34:40 +0100

    modify Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <Router>
-      <Routes basename={process.env.REACT_APP_PUBLIC_URL}>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
